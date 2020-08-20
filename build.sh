83b69a029be9c53bb7915519affd29353cb32a50
metfan1981 <metfan1981@example.com>
2020-08-20 17:52:40 +0300

    added conditions to build.sh to retry playbook running

--- build.sh
+++ build.sh
@@ -7,7 +7,16 @@ cd $TF_DIR
 terraform init
 terraform apply -auto-approve
 
+# Parsing terraform.tfstate to gather hosts' IP addresses and create ansible inventory
 cd $WORKDIR
 python parser-tfstate.py -p $TF_DIR
 cd ansible
+
 ansible-playbook web-db.yml -i $TF_DIR/tf-inventory.ini --ask-vault-pass
+
+# may not work after initial playbook run
+if [ $? -ne 0 ]; then
+  ansible-playbook web-db.yml -i $TF_DIR/tf-inventory.ini --ask-vault-pass
+fi
+
+echo "Instances have been provisioned and configured successfully!"
